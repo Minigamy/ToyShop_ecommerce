103c33c56530810aa44de0af013da1514a09aa9d
Minigamy <ferdinand-f@mail.ru>
2022-08-26 12:45:32 +0300

    Add recommendation in cart page

--- cart/views.py
+++ cart/views.py
@@ -5,6 +5,7 @@ from coupons.forms import CouponApplyForm
 from store.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from store.recommender import Recommender
 
 
 @require_POST
@@ -35,4 +36,9 @@ def cart_detail(request):
             'quantity': item['quantity'],
             "override": False})
     coupon_apply_form = CouponApplyForm()
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products, max_results=4)
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form,
+                                                'recommended_products': recommended_products})
